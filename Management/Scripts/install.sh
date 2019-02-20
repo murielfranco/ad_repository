33fad8f48f85dbd21cf62e573e68b8833eae678b
Muriel F. Franco <franco@ifi.uzh.ch>
2019-02-20 18:56:51 +0100

    Initial configurations for the VNF

--- Management/Scripts/install.sh
+++ Management/Scripts/install.sh
@@ -3,7 +3,7 @@ set -e
 echo "Installing Pi-hole..."
 
 # Install Pi-hole
-cd ../Resources && touch setupVars.conf && sudo chmod +x get_ip_addr.sh  &&  sudo ./get_ip_addr.sh ens4
+cd ../Resources && sudo chmod +x get_ip_addr.sh  &&  sudo ./get_ip_addr.sh ens4
 sudo mkdir -p /etc/pihole/
 cd ../Resources/ && sudo cp setupVars.conf /etc/pihole
 curl -L https://install.pi-hole.net | sudo bash /dev/stdin --unattended
