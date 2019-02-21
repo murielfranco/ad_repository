0ff5fa6ee06daf0e2c0f392ec35cace2b0b0f90e
Muriel F. Franco <franco@ifi.uzh.ch>
2019-02-21 15:27:22 +0100

    Correção no instalador da função

--- Management/Scripts/install.sh
+++ Management/Scripts/install.sh
@@ -3,9 +3,11 @@ set -e
 echo "Installing Pi-hole..."
 
 # Install Pi-hole
-cd ../Resources && sudo chmod +x get_ip_addr.sh  &&  sudo ./get_ip_addr.sh ens4
+cd Management/Resources && sudo chmod +x get_ip_addr.sh  &&  sudo ./get_ip_addr.sh ens4
 sudo mkdir -p /etc/pihole/
-cd ../Resources/ && sudo cp setupVars.conf /etc/pihole
+cd Management/Resources/ && sudo cp setupVars.conf /etc/pihole
+
+
 curl -L https://install.pi-hole.net | sudo bash /dev/stdin --unattended
 
 
