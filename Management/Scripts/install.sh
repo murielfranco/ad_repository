b0d03b66e349a2825c6972efc68e4ec177591438
Muriel F. Franco <franco@ifi.uzh.ch>
2019-03-06 16:38:57 +0100

    Updating install file

--- Management/Scripts/install.sh
+++ Management/Scripts/install.sh
@@ -3,12 +3,10 @@ set -e
 echo "Installing Pi-hole..."
 
 # Install Pi-hole
-cd Management/Resources && sudo chmod +x get_ip_addr.sh  &&  sudo ./get_ip_addr.sh ens4
+cd Management/Resources && sudo chmod +x get_ip_addr.sh  &&  sudo ./get_ip_addr.sh ens4 
 sudo mkdir -p /etc/pihole/
-cd Management/Resources/ && sudo cp setupVars.conf /etc/pihole
-
-
-curl -L https://install.pi-hole.net | sudo bash /dev/stdin --unattended
-
+sudo cp setupVars.conf /etc/pihole
+sudo curl -L https://install.pi-hole.net | sudo bash /dev/stdin --unattended
+cd ~
 
 
