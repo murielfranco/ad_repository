6762cea500facfe97176b5ceb59745de21fefc3a
Muriel F. Franco <franco@ifi.uzh.ch>
2019-03-06 20:04:00 +0100

    Changes on the setupVars

--- Management/Scripts/install.sh
+++ Management/Scripts/install.sh
@@ -4,4 +4,4 @@ echo "Installing Pi-hole..."
 
 # Install Pi-hole
 cd Management/Resources && sudo chmod +x get_ip_addr.sh && sudo ./get_ip_addr.sh ens4 && sudo mkdir -p /etc/pihole/ && sudo cp /home/fende/Management/Resources/setupVars.conf /etc/pihole
-sudo curl -L https://install.pi-hole.net | sudo bash /dev/stdin --unattended
+sudo curl -sSl https://install.pi-hole.net | sudo bash /dev/stdin --unattended
