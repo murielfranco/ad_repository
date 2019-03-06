65fcb589af77391c1d45cfd5523eb75bab9c88bc
Giovanni Venâncio <gvs11ufpr@gmail.com>
2019-03-06 14:28:01 -0300

    Add absolute path on setupVars.conf
    Signed-off-by: Giovanni Venâncio <gvs11ufpr@gmail.com>

--- Management/Scripts/install.sh
+++ Management/Scripts/install.sh
@@ -3,8 +3,5 @@ set -e
 echo "Installing Pi-hole..."
 
 # Install Pi-hole
-cd Management/Resources && sudo chmod +x get_ip_addr.sh && sudo ./get_ip_addr.sh ens4 && sudo mkdir -p /etc/pihole/ && sudo cp setupVars.conf /etc/pihole 
+cd Management/Resources && sudo chmod +x get_ip_addr.sh && sudo ./get_ip_addr.sh ens4 && sudo mkdir -p /etc/pihole/ && sudo cp /home/fende/Management/Resources/setupVars.conf /etc/pihole
 sudo curl -L https://install.pi-hole.net | sudo bash /dev/stdin --unattended
-
-
-
