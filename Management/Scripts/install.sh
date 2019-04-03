7679b5c8cc437e1a17827afc05307b5ef5b93da9
Giovanni Venâncio <gvs11ufpr@gmail.com>
2019-04-03 11:28:30 -0300

    Adicionado encaminhamento IPV4
    Signed-off-by: Giovanni Venâncio <gvs11ufpr@gmail.com>

--- Management/Scripts/install.sh
+++ Management/Scripts/install.sh
@@ -2,6 +2,8 @@
 set -e
 echo "Installing Pi-hole..."
 
+sudo sh -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
+
 # Install Pi-hole and requirements
 cd Management/Resources && sudo chmod +x get_ip_addr.sh && sudo ./get_ip_addr.sh ens4 && sudo mkdir -p /etc/pihole/ && sudo cp /home/fende/Management/Resources/setupVars.conf /etc/pihole
 sudo curl -L https://install.pi-hole.net | sudo bash /dev/stdin --unattended
